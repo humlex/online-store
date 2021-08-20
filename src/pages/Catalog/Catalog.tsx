ede51cde43c4ed54168b29ad8ef774eed8df70b7
clairobscour <clairobscour@gmail.com>
2021-08-20 10:28:30 +0300

    feat: added basic pages

---
+++ src/pages/Catalog/Catalog.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Catalog = () => <>It`s Catalog page</>;
+
+export default Catalog;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Home = () => <>It`s Home page</>;
+
+export default Home;
